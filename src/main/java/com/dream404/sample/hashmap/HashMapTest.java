a1936d158326b017c777b960626d13a3a6e5c3ba
wuxuping <wuxupingvip@126.com>
2016-08-29 16:46:20 +0800

    HashMap

---
+++ src/main/java/com/dream404/sample/hashmap/HashMapTest.java
@@ -0,0 +1,16 @@
+package com.dream404.sample.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description
+ * @author xuping
+ * @date 2016年8月28日
+ *
+ */
+public class HashMapTest {
+	public static void main(String[] args) {
+		Map<Object,Object> map = new HashMap<Object,Object>();
+	}
+}
