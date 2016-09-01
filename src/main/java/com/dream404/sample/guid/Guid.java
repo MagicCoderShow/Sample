b166e1359a86c861d1347b92dee34ddaa5efce89
xuping <wuxupingvip@126.com>
2016-09-01 18:35:54 +0800

    修改版

--- src/main/java/com/dream404/sample/guid/Guid.java
+++ src/main/java/com/dream404/sample/guid/Guid.java
@@ -2,127 +2,122 @@ package com.dream404.sample.guid;
 
 /**
  * GUID工具类
+ * 
  * @author 旭平
  *
  */
 @SuppressWarnings("static-access")
 public class Guid {
 
+	// 机器标识
 	private final long workerId;
+	// 数据中心
+	private final long datacenterId;
+	// 时间标记
+	private final static long twepoch = 1472720292666L;
+	// 机器标识位数
+	private final static long workerIdBits = 5L;
+	// 数据中心标识位数
+	private final static long datacenterIdBits = 5L;
+	// 机器ID最大值
+	private final static long maxWorkerId = -1L ^ (-1L << workerIdBits);
+	// 数据中心ID最大值
+	private final static long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+	// 毫秒内自增位
+	private final static long sequenceBits = 12L;
+	// 机器ID偏左移12位
+	private final static long workerIdShift = sequenceBits;
+	// 数据中心ID左移17位
+	private final static long datacenterIdShift = sequenceBits + workerIdBits;
+	// 时间毫秒左移22位
+	private final static long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
 
-	private final static long twepoch = 1361753741828L;
+	private final static long sequenceMask = -1L ^ (-1L << sequenceBits);
+
+	private static long lastTimestamp = -1L;
 
 	private long sequence = 0L;
 
-	private final static long workerIdBits = 4L;
-
-	public final static long maxWorkerId = -1L ^ -1L << workerIdBits;
-
-	private final static long sequenceBits = 10L;
-
-	private final static long workerIdShift = sequenceBits;
-
-	private final static long timestampLeftShift = sequenceBits + workerIdBits;
-
-	public final static long sequenceMask = -1L ^ -1L << sequenceBits;
-
-	private long lastTimestamp = -1L;
-
-	public Guid(final long workerId) {
-		super();
-		if (workerId > this.maxWorkerId || workerId < 0) {
-
-			throw new IllegalArgumentException(String.format(
-
-					"worker Id can't be greater than %d or less than 0",
-
-					this.maxWorkerId));
-
+	/**
+	 * 构造
+	 * 
+	 * @param workerId
+	 *            机器标识ID
+	 * @param datacenterId
+	 *            数据中心ID
+	 */
+	public Guid(long workerId, long datacenterId) {
+		if (workerId > maxWorkerId || workerId < 0) {
+			throw new IllegalArgumentException(String.format("机器标识ID范围在0-31"));
 		}
-
+		if (datacenterId > maxDatacenterId || datacenterId < 0) {
+			throw new IllegalArgumentException("数据中心范围在0-31");
+		}
 		this.workerId = workerId;
-
+		this.datacenterId = datacenterId;
 	}
 
+	/**
+	 * 下一个ID
+	 * 
+	 * @return
+	 */
 	public synchronized long nextId() {
-
-		long timestamp = this.timeGen();
-
-		if (this.lastTimestamp == timestamp) {
-
-			this.sequence = (this.sequence + 1) & this.sequenceMask;
-
-			if (this.sequence == 0) {
-
-				// System.out.println("###########" + sequenceMask);
-
-				timestamp = this.tilNextMillis(this.lastTimestamp);
-
-			}
-
-		} else {
-
-			this.sequence = 0;
-
-		}
-
-		if (timestamp < this.lastTimestamp) {
-
+		long timestamp = timeGen();
+		if (timestamp < lastTimestamp) {
 			try {
-
-				throw new Exception(
-
-						String.format(
-
-								"Clock moved backwards.  Refusing to generate id for %d milliseconds",
-
-								this.lastTimestamp - timestamp));
-
+				throw new Exception("时间向后移动，拒绝生成这个" + (lastTimestamp - timestamp) + "毫秒数的id");
 			} catch (Exception e) {
-
 				e.printStackTrace();
-
 			}
-
 		}
 
-		this.lastTimestamp = timestamp;
-
-		long nextId = ((timestamp - twepoch << timestampLeftShift))
-
-				| (this.workerId << this.workerIdShift) | (this.sequence);
-
-		// System.out.println("timestamp:" + timestamp + ",timestampLeftShift:"
-
-		// + timestampLeftShift + ",nextId:" + nextId + ",workerId:"
-
-		// + workerId + ",sequence:" + sequence);
+		if (lastTimestamp == timestamp) {
+			// 当前毫秒内，则+1
+			sequence = (sequence + 1) & sequenceMask;
+			if (sequence == 0) {
+				// 当前毫秒内计数满了，则等待下一秒
+				timestamp = tilNextMillis(lastTimestamp);
+			}
+		} else {
+			sequence = 0;
+		}
+		lastTimestamp = timestamp;
+		// ID偏移组合生成最终的ID，并返回ID
+		long nextId = ((timestamp - twepoch) << timestampLeftShift) | (datacenterId << datacenterIdShift)
+				| (workerId << workerIdShift) | sequence;
 
 		return nextId;
-
 	}
 
+	/**
+	 * 下一毫秒
+	 * 
+	 * @param lastTimestamp
+	 * @return
+	 */
 	private long tilNextMillis(final long lastTimestamp) {
-
 		long timestamp = this.timeGen();
 		while (timestamp <= lastTimestamp) {
-
 			timestamp = this.timeGen();
-
 		}
-
 		return timestamp;
-
 	}
 
+	/**
+	 * 当前时间
+	 * 
+	 * @return
+	 */
 	private long timeGen() {
-
 		return System.currentTimeMillis();
-
 	}
 
 	public static void main(String[] args) {
-		Thread td = new Thread();
-		td.getId();
+		final Guid guid = new Guid(0, 0);
+		for (int i = 0; i < 2; i++) {
+			System.out.println(guid.nextId());
+		}
+		System.out.println("-------------");
 	}
 }
